19417f4fda0d5880e022eef41975047e55eace8a
Advait Shinde <advait.shinde@gmail.com>
2026-01-20 21:42:18 +0000

    Add noexecstack linker flag for CGO

--- go-ghostty/cgo_flags_asan.go
+++ go-ghostty/cgo_flags_asan.go
@@ -6,5 +6,6 @@ package ghostty
 /*
 #cgo CFLAGS: -I${SRCDIR}/shim/zig-out-asan/include
 #cgo LDFLAGS: -L${SRCDIR}/shim/zig-out-asan/lib -lvtr-ghostty-vt
+#cgo linux LDFLAGS: -Wl,-z,noexecstack
 */
 import "C"
--- go-ghostty/cgo_flags_default.go
+++ go-ghostty/cgo_flags_default.go
@@ -6,5 +6,6 @@ package ghostty
 /*
 #cgo CFLAGS: -I${SRCDIR}/shim/zig-out/include
 #cgo LDFLAGS: -L${SRCDIR}/shim/zig-out/lib -lvtr-ghostty-vt
+#cgo linux LDFLAGS: -Wl,-z,noexecstack
 */
 import "C"
