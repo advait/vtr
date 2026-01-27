42d3db572def0150f6f728f7b2305f743420fcd9
Advait Shinde <advait.shinde@gmail.com>
2026-01-27 23:36:30 +0000

    web: reduce nerd font glyph swap

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,6 +6,29 @@ import App from "./App";
 import { loadPreferences } from "./lib/preferences";
 import { applyTheme, getTheme } from "./lib/theme";
 
+const nerdFontRegularUrl = new URL(
+  "./assets/fonts/jetbrains-mono-nerd/JetBrainsMonoNerdFont-Regular.ttf",
+  import.meta.url,
+).href;
+
+function preloadFont(href: string, type: string) {
+  if (typeof document === "undefined") {
+    return;
+  }
+  if (document.querySelector(`link[rel="preload"][href="${href}"]`)) {
+    return;
+  }
+  const link = document.createElement("link");
+  link.rel = "preload";
+  link.as = "font";
+  link.type = type;
+  link.href = href;
+  link.crossOrigin = "anonymous";
+  document.head.appendChild(link);
+}
+
+preloadFont(nerdFontRegularUrl, "font/ttf");
+
 applyTheme(getTheme(loadPreferences().themeId));
 
 const root = document.getElementById("root");
