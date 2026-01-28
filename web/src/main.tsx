c4dddf95dcee0d47931c919a7a41e50b57dd23a6
Advait Shinde <advait.shinde@gmail.com>
2026-01-28 00:09:54 +0000

    web: use nerd font for all glyphs

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "@fontsource/jetbrains-mono/latin.css";
 import "./styles.css";
 import App from "./App";
 import { loadPreferences } from "./lib/preferences";
